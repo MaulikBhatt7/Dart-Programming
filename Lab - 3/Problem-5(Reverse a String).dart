8163923ca5b2ba2225954ee52ba136a9e9ef9887
Maulik Bhatt <101645973+MaulikBhatt7@users.noreply.github.com>
2022-12-16 12:26:31 +0530

    Update Problem-5(Reverse a String).dart

--- Lab - 3/Problem-5(Reverse a String).dart
+++ Lab - 3/Problem-5(Reverse a String).dart
@@ -3,5 +3,5 @@ import 'dart:io';
 void main(List<String> args) {
   stdout.write("Enter String : ");
   String str = stdin.readLineSync()!;
-  print(str.split("").reversed.toString());
+  print(str.split("").reversed.join());
 }
